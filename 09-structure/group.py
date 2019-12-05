2173485d3c8ebab160dce181ee5f2e8dda31aa20
Graham Van Goffrier <vangoffrier@gmail.com>
2019-12-05 11:20:11 +0000

    Made group a local variable and adjusted functions to match.

--- 09-structure/group.py
+++ 09-structure/group.py
@@ -1,50 +1,20 @@
-group = {
-    "Jill": {
-        "age": 26,
-        "job": "biologist",
-        "relations": {
-            "Zalika": "friend",
-            "John": "partner"
-        }
-    },
-    "Zalika": {
-        "age": 28,
-        "job": "artist",
-        "relations": {
-            "Jill": "friend"
-        }
-    },
-    "John": {
-        "age": 27,
-        "job": "writer",
-        "relations": {
-            "Jill": "partner"
-        }
-    },
-    "Nash": {
-        "age": 34,
-        "job": "chef",
-        "relations": {
-            "John": "cousin",
-            "Zalika": "landlord"
-        }
-    }
-}
+#def average_age():
+#    """Compute the average age of the group's members."""
+#    all_ages = [person["age"] for person in group.values()]
+#    return sum(all_ages) / len(group)
 
-
-def average_age():
-    """Compute the average age of the group's members."""
+def average_age(group):
     all_ages = [person["age"] for person in group.values()]
     return sum(all_ages) / len(group)
 
 
-def forget(person1, person2):
+def forget(group, person1, person2):
     """Remove the connection between two people."""
     group[person1]["relations"].pop(person2, None)
     group[person2]["relations"].pop(person1, None)
 
 
-def add_person(name, age, job, relations):
+def add_person(group, name, age, job, relations):
     """Add a new person with the given characteristics to the group."""
     new_person = {
         "age": age,
@@ -55,7 +25,41 @@ def add_person(name, age, job, relations):
 
 
 if __name__ == "__main__":
-    print("The group has {} members with an average age of {}".format(
+
+	group = {
+		"Jill": {
+		    "age": 26,
+		    "job": "biologist",
+		    "relations": {
+		        "Zalika": "friend",
+		        "John": "partner"
+		    }
+		},
+		"Zalika": {
+		    "age": 28,
+		    "job": "artist",
+		    "relations": {
+		        "Jill": "friend"
+		    }
+		},
+		"John": {
+		    "age": 27,
+		    "job": "writer",
+		    "relations": {
+		        "Jill": "partner"
+		    }
+		},
+		"Nash": {
+		    "age": 34,
+		    "job": "chef",
+		    "relations": {
+		        "John": "cousin",
+		        "Zalika": "landlord"
+		    }
+		}
+	}
+
+	print("The group has {} members with an average age of {}".format(
         len(group),
-        average_age()
-    ))
+        average_age(group)
+	))
